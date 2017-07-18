4d8df4e6283a359d5accf969ba464548fbddd99d
AP <ap@contoso.com>
2017-07-18 12:42:48 -0500

    added delete route

--- server.js
+++ server.js
@@ -75,6 +75,21 @@ body.description = body.description.trim();
 
 
 
+//DELTE ID
+
+app.delete('/todos/:id', function(req, res){
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	
+	if (!matchedTodo){
+		res.status(404).json({"error": "no todo found with that id"});
+		} else {
+			todos = _.without(todos, matchedTodo);
+			res.json(matchedTodo);
+			}
+	
+	});
+
 
 app.listen(PORT, function(){
 	console.log('express listening on port ' + PORT + ' ! ');
