962d669190f1123c97fbd45eb20fbeb7e179d747
AP <ap@contoso.com>
2017-07-19 19:42:04 -0500

    added put todos

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.post('/todos', function(req, res){
 console.log('hello post');
 
  //var body = req.body;  // use _.pick
- var body = _.pick(req.body, 'description', 'completed')
+ var body = _.pick(req.body, 'description', 'completed');
 
  if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
 	 return res.status(400).send();
@@ -89,6 +89,50 @@ app.delete('/todos/:id', function(req, res){
 			}
 	
 	});
+	
+
+
+//UPDATEing IDs using PUT
+
+app.put('/todos/:id', function(req, res){
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+	
+   if(!matchedTodo) {
+	   return res.status(404).send();
+	   }
+	
+	
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+		} else if (body.hasOwnProperty('completed')){
+			// bad.  not a boolean
+				return res.status(400).send();
+				
+			} 
+			
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+		}
+	
+	// HERE is good to update.
+
+
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
+	
+	
+	
+	
+	
+	
+	});
+
+
 
 
 app.listen(PORT, function(){
